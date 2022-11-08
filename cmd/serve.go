ef8da1b0d83a96be75ae450a598d1d261140caf6
Vadim Galaktionov <vadim@galaktionov.nl>
2022-11-08 13:31:00 +0000

    implement hot-reloading dev server

--- cmd/serve.go
+++ cmd/serve.go
@@ -10,19 +10,74 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	esbuild "github.com/evanw/esbuild/pkg/api"
+	"github.com/radovskyb/watcher"
 )
 
-func init() {
+func buildGo() {
 	os.Setenv("GOOS", "js")
 	os.Setenv("GOARCH", "wasm")
 	cmd := exec.Command("go", "build", "-o", "./assets/main.wasm")
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Println(err.Error())
+	} else {
+		log.Println("built .go sources to ./assets/main.wasm")
 	}
 }
 
-func main() {
+func buildTs() {
+	result := esbuild.Build(esbuild.BuildOptions{
+		EntryPoints: []string{"./draw/renderer.ts"},
+		Bundle:      true,
+		Outfile:     "./assets/renderer.js",
+		Write:       true,
+	})
+
+	if len(result.Errors) > 0 {
+		log.Println(result.Errors)
+	} else {
+		log.Println("built .ts sources to ./assets/renderer.js")
+	}
+}
+
+func watchSourceFiles() {
+	w := watcher.New()
+	w.SetMaxEvents(1)
+	r := regexp.MustCompile(`(\.go|\.ts)$`)
+	w.AddFilterHook(watcher.RegexFilterHook(r, false))
+
+	go func() {
+		for {
+			select {
+			case event := <-w.Event:
+				if strings.HasSuffix(event.Name(), ".go") {
+					buildGo()
+				} else if strings.HasSuffix(event.Name(), ".ts") {
+					buildTs()
+				}
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				return
+			}
+		}
+	}()
+
+	if err := w.AddRecursive("."); err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := w.Start(time.Millisecond * 100); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func serve() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		if req.RequestURI == "/" {
 			req.RequestURI = "/index.html"
@@ -34,4 +89,13 @@ func main() {
 	})
 	log.Println("Serving on port 1334...")
 	log.Println(http.ListenAndServe(":1334", nil))
+}
+
+func main() {
+	buildGo()
+	buildTs()
+
+	go watchSourceFiles()
+
+	serve()
 }
--- go.mod
+++ go.mod
@@ -3,21 +3,24 @@ module github.com/vgalaktionov/roguelike-go
 go 1.19
 
 require (
+	github.com/bits-and-blooms/bitset v1.2.0
 	github.com/gdamore/tcell/v2 v2.5.3
 	github.com/norendren/go-fov v1.0.1
 	github.com/stretchr/testify v1.8.1
-	github.com/bits-and-blooms/bitset v1.2.0
 )
 
 require (
-	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/evanw/esbuild v0.15.13
+	github.com/fsnotify/fsnotify v1.6.0
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/radovskyb/watcher v1.0.7
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/solarlune/paths v0.0.0-20220620061549-ff4d7ca73cb7
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
