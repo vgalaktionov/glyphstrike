10267dae881420ecf1b29ff011c92f7d2f5d1ca2
Vadim Galaktionov <vadim@galaktionov.nl>
2022-11-23 23:29:53 +0100

    wip pyxel

--- ecs/component.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC
-from dataclasses import dataclass
-
-
-@dataclass
-class Component(ABC):
-    pass
--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 import pyxel
+import esper
 
+ASSETS = 'assets.pyxres'
+
+
+def init():
+    pyxel.init(160, 120)
+    pyxel.load(ASSETS)
+
+
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+
+
+def draw():
+    pyxel.cls(0)
+    pyxel.rect(10, 10, 20, 20, 11)
+    pyxel.text(40, 40, "Hello World", 1)
+
+
+if __name__ == '__main__':
+    init()
+    pyxel.run(update, draw)
